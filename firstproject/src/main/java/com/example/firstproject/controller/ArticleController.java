c2d2fbf877b1ff7d853c576e7282e58558d46185
ctsiwonnim <siwonnima@gmail.com>
2024-06-08 20:52:35 +0900

    페이지 간 이동

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
@@ -31,7 +35,26 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
         //System.out.println(saved.toString());
-        return "";
+        return "redirect:/articles/"+saved.getId();
         //144p까지
+    }
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id = "+ id);
+        //id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //모델에 데이터 등록학
+        model.addAttribute("article", articleEntity);
+        //뷰 페이지 반환하기
+        return "articles/show";
+    }
+    @GetMapping("/articles")
+    public String index(Model model){
+        //모든 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        //모델에 데이터 등록하기
+        model.addAttribute("articleList",articleEntityList);
+        // 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
